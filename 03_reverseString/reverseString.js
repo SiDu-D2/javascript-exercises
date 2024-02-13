b0c23759e460198e07b4b3f7d7b9e17bc31371a1
Simon Duff <64978006+SiDu-D2@users.noreply.github.com>
2024-02-13 11:06:26 +0000

    Update reverseString.js exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  let array = word.split('');
+  let reverseArray = array.reverse();
+  let reverseStr = reverseArray.join('');
+  return reverseStr;
 };
 
 // Do not edit below this line
