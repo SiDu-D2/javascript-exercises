132e9082d9f433288fb3753966a4e7507a74158a
Simon Duff <64978006+SiDu-D2@users.noreply.github.com>
2024-03-14 16:35:00 +0000

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a,b) {
+  let sum = 0;
+  if (a > b) {
+    [a, b] = [b, a];
+  }
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
