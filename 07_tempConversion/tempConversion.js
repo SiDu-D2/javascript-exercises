d14d0034a05bb74be854233a55bc28627cd49c38
Simon Duff <64978006+SiDu-D2@users.noreply.github.com>
2024-03-15 11:30:46 +0000

    Update tempConversion - exercise complete.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (x) {
+  let Celsius = (x - 32) * (5 / 9);
+  let rounded = Math.round(Celsius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (x) {
+  let Fahrenheit = x * (9 / 5) + 32;
+  return Math.round(Fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
